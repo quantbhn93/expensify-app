87aebed2396f010df6a270835a0bc511b21e2b50
Quân Chùa <quantb.hn93@gmail.com>
2020-05-08 06:31:52 +0700

    Extract text plugin for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
 
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('style.css');
 
   return {
     entry: './src/app.js',
@@ -20,14 +22,24 @@ module.exports = (env) => {
         },
         {
           test: /\.s?css$/,  // load css file and scss file
-          use: [
-            'style-loader', //  2. attach to the DOM
-            'css-loader', // 1. read css file from source
-            'sass-loader' // 0. load sass into css (use node sass to load file)
-          ]
+          use: CSSExtract.extract({
+            use: [
+              'css-loader',
+              'sass-loader'
+            ]
+          })
+          // use: [
+          //   'style-loader', //  2. attach inline to the DOM
+          //   'css-loader', // 1. read css file from source
+          //   'sass-loader' // 0. load sass into css (use node sass to load file)
+          // ]
         }
       ]
     },
+    plugins: [
+      CSSExtract
+    ],
+    // setup devtool: source map for Javascript
     devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
