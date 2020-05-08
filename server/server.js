65fb6fb87175a0249bbf3f59ac51faa41ae8cb75
Quan Tran <quan.tran@episerver.com>
2020-05-08 09:57:02 +0700

    Setup production build and server for Heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000; // if deoloy on Heroku => process.env.PORT has value
 
 app.use(express.static(publicPath));
 
@@ -9,6 +10,6 @@ app.get('*', (req, res) => {
     res.sendfile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up !');
 });
